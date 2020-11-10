f4dab94862e86c2c0c968f0fc90ed7fdc86572df
Christoph Bohne christoph-bohne@gmx.de <christoph-bohne@gmx.de>
2020-11-10 16:50:37 +0100

    Added this piece of shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+task_one = Task.new(title: "Finish this exercise", details: "Read the instructions")
+task_one.save!
+task_two = Task.new(title: "Finish the next exercise", details: "Read the instructions")
+task_two.save!
